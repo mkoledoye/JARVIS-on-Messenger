0bf4120710c10474f34c63848f12e82e3debdd05
mo_sees <moses.koledoye01@universitadipavia.it>
2016-04-24 01:50:45 +0200

    edit test query

--- modules/tests/test_music.py
+++ modules/tests/test_music.py
@@ -2,6 +2,6 @@ import modules
 
 def test_movie():
     assert('music' == modules.process_query('ophelia song')[0])
-    assert('music' == modules.process_query('who sang Hold You Up')[0])
+    assert('music' == modules.process_query('who sang Hold You Up?')[0])
     assert('music' == modules.process_query('When was the Fast Car song released?')[0])
     assert('music' != modules.process_query('something random')[0])
